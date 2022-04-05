be736b02f5419fce451765f5f9fb389ab0d8a39a
simplephi <cleverdarkness@gmail.com>
2022-04-05 23:01:26 +0800

    test add camera position

--- aframe/examples/marker-based/js/multiMarkerstorusworld.js
+++ aframe/examples/marker-based/js/multiMarkerstorusworld.js
@@ -138,6 +138,24 @@ AFRAME.registerComponent('markers_start',{
 });
 
 
+// Reading World Position or Rotation of the Camera
+AFRAME.registerComponent('rotation-reader', {
+  /**
+   * We use IIFE (immediately-invoked function expression) to only allocate one
+   * vector or euler and not re-create on every tick to save memory.
+   */
+  tick: (function () {
+    var position = new THREE.Vector3();
+    var quaternion = new THREE.Quaternion();
+
+    return function () {
+      this.el.object3D.getWorldPosition(position);
+      this.el.object3D.getWorldQuaternion(quaternion);
+      // position and rotation now contain vector and quaternion in world space.
+    };
+  })()
+});
+
 //Detect marker found and lost
 AFRAME.registerComponent('registerevents', {
 		init: function () {
