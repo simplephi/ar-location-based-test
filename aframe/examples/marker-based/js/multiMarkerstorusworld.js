21d576f7c8b6c61387f8af8c042820fde4751c8f
simplephi <cleverdarkness@gmail.com>
2022-04-07 01:59:07 +0800

    android debug

--- aframe/examples/marker-based/js/multiMarkerstorusworld.js
+++ aframe/examples/marker-based/js/multiMarkerstorusworld.js
@@ -148,15 +148,15 @@ AFRAME.registerComponent('markers_start',{
 						console.log(markerEl);
 				}
 
-				var cameraEl = document.querySelector('#camera');
-				var worldPos = new THREE.Vector3();
-				worldPos.setFromMatrixPosition(cameraEl.object3D.matrixWorld);
-				console.log("world pos: ", worldPos.x);
-
-				cameraEl.addEventListener('componentchanged', function (evt) {
-				  if (evt.detail.name !== 'position') { return; }
-				  console.log("componentchanged : ", evt.detail.newData);
-				});
+				// var cameraEl = document.querySelector('#camera');
+				// var worldPos = new THREE.Vector3();
+				// worldPos.setFromMatrixPosition(cameraEl.object3D.matrixWorld);
+				// console.log("world pos: ", worldPos.x);
+				//
+				// cameraEl.addEventListener('componentchanged', function (evt) {
+				//   if (evt.detail.name !== 'position') { return; }
+				//   console.log("componentchanged : ", evt.detail.newData);
+				// });
 						// console.log('Model component registered successfully!' , ` latitude: ${position.coords.latitude}; longitude: ${position.coords.longitude}`);
 
 
