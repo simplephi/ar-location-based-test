be9cce117c6ba1faa327ea5df93de38adfe4ff3d
simplephi <cleverdarkness@gmail.com>
2022-04-11 12:33:49 +0800

    sphere oldest

--- aframe/examples/marker-based/js/multiMarkerstorusworld.js
+++ aframe/examples/marker-based/js/multiMarkerstorusworld.js
@@ -31,129 +31,129 @@ AFRAME.registerComponent('markers_start',{
 
 
 
-						//Adding element
-						var spiral = document.createElement('a-entity');
-
-
-						var torus1_parent = document.createElement('a-entity');
-						var torus1 = document.createElement('a-entity');
-						var sphere1 = document.createElement('a-entity');
-
-						var torus2_parent = document.createElement('a-entity');
-						var torus2 = document.createElement('a-entity');
-						var sphere2 = document.createElement('a-entity');
-
-						var torus3_parent = document.createElement('a-entity');
-						var torus3 = document.createElement('a-entity');
-						var sphere3 = document.createElement('a-entity');
-
-						// OBJ model
-
-						// Spiral
-						// textEl.setAttribute('gltf-model','#monster');
-						// spiral.setAttribute('gps-entity-place', `latitude: ${position.coords.latitude}; longitude: ${position.coords.longitude}`);
-						// spiral.setAttribute('gps-entity-place', `latitude: ${lat}; longitude: ${long}`);
-						spiral.setAttribute('obj-model', {
-							obj: '#spiral-obj',
-							mtl: '#spiral-mtl'
-						});
-						spiral.object3D.position.set(0, 0, 0);
-						spiral.object3D.scale.set(0.1, 0.1, 0.1);
-						spiral.object3D.rotation.set(0, 0, 0);
-						spiral.emit('rotate');
-
-						markerEl.appendChild(spiral);
-
-
-						// Torus 1
-						// textEl.setAttribute('gltf-model','#monster');
-						// textEl.setAttribute('gps-entity-place', `latitude: ${position.coords.latitude}; longitude: ${position.coords.longitude}`);
-						// textEl.setAttribute('gps-entity-place', `latitude: ${lat}; longitude: ${long}`);
-
-						torus1.setAttribute('obj-model', {
-							obj: '#torus_gold_oldest-obj',
-							mtl: '#torus_gold_oldest-mtl'
-						});
-						torus1.object3D.position.set(1, 0, 0);
-						torus1.object3D.scale.set(0.1, 0.1, 0.1);
-						torus1.object3D.rotation.set(0, 0, 0);
-
-
-						sphere1.setAttribute('obj-model', {
-							obj: '#sphere_gold_oldest-obj',
-							mtl: '#sphere_gold_oldest-mtl'
-						});
-						sphere1.object3D.position.set(1, 0, 0);
-						sphere1.object3D.scale.set(0.05, 0.05, 0.05);
-						sphere1.object3D.rotation.set(0, 0, 0);
-
-						sphere1.setAttribute('animation', {
-						  property: 'rotation',
-						  to: '0 360 0',
-						  loop: true,
-							dur: 2000
-						});
-
-
-						markerEl.appendChild(torus1);
-						markerEl.appendChild(sphere1);
-						// markerEl.appendChild(torus1_parent);
-
-
-						// Torus 2
-						// textEl1.setAttribute('gps-entity-place', `latitude: ${position.coords.latitude}; longitude: ${position.coords.longitude}`);
-						// textEl1.setAttribute('gps-entity-place', `latitude: ${lat}; longitude: ${long}`);
-						torus2.setAttribute('obj-model', {
-							obj: '#torus_gold_oldest-obj',
-							mtl: '#torus_gold_oldest-mtl'
-						});
-						torus2.object3D.position.set(1.3, 0, 0);
-						torus2.object3D.scale.set(0.2, 0.2, 0.2);
-						torus2.object3D.rotation.set(0, 0, 0);
-
-						sphere2.setAttribute('obj-model', {
-							obj: '#sphere_gold_oldest-obj',
-							mtl: '#sphere_gold_oldest-mtl'
-						});
-						sphere2.object3D.position.set(1.3, 0, 0);
-						sphere2.object3D.scale.set(0.05, 0.05, 0.05);
-						sphere2.object3D.rotation.set(0, 0, 0);
-
-
-						markerEl.appendChild(torus2);
-						markerEl.appendChild(sphere2);
-						// markerEl.appendChild(torus2_parent);
-
-
-
-						// Torus 3
-						// textEl2.setAttribute('gps-entity-place', `latitude: ${position.coords.latitude}; longitude: ${position.coords.longitude}`);
-						// textEl2.setAttribute('gps-entity-place', `latitude: ${lat}; longitude: ${long}`);
-						torus3.setAttribute('obj-model', {
-							obj: '#torus_gold_oldest-obj',
-							mtl: '#torus_gold_oldest-mtl'
-						});
-						torus3.object3D.position.set(1.6, 0, 0);
-						torus3.object3D.scale.set(0.3, 0.3, 0.3);
-						torus3.object3D.rotation.set(0, 0, 0);
-
-						sphere3.setAttribute('obj-model', {
-							obj: '#sphere_gold_oldest-obj',
-							mtl: '#sphere_gold_oldest-mtl'
-						});
-						sphere3.object3D.position.set(1.6, 0, 0);
-						sphere3.object3D.scale.set(0.05, 0.05, 0.05);
-						sphere3.object3D.rotation.set(0, 0, 0);
-
-
-						markerEl.appendChild(torus3);
-						markerEl.appendChild(sphere3);
-						// markerEl.appendChild(torus3_parent);
-
-
-						// console.log('Model component registered successfully!' , ` latitude: ${lat}; longitude: ${long}`);
-						console.log('Model component registered successfully!' );
-						console.log(markerEl);
+						// //Adding element
+						// var spiral = document.createElement('a-entity');
+						//
+						//
+						// var torus1_parent = document.createElement('a-entity');
+						// var torus1 = document.createElement('a-entity');
+						// var sphere1 = document.createElement('a-entity');
+						//
+						// var torus2_parent = document.createElement('a-entity');
+						// var torus2 = document.createElement('a-entity');
+						// var sphere2 = document.createElement('a-entity');
+						//
+						// var torus3_parent = document.createElement('a-entity');
+						// var torus3 = document.createElement('a-entity');
+						// var sphere3 = document.createElement('a-entity');
+						//
+						// // OBJ model
+						//
+						// // Spiral
+						// // textEl.setAttribute('gltf-model','#monster');
+						// // spiral.setAttribute('gps-entity-place', `latitude: ${position.coords.latitude}; longitude: ${position.coords.longitude}`);
+						// // spiral.setAttribute('gps-entity-place', `latitude: ${lat}; longitude: ${long}`);
+						// spiral.setAttribute('obj-model', {
+						// 	obj: '#spiral-obj',
+						// 	mtl: '#spiral-mtl'
+						// });
+						// spiral.object3D.position.set(0, 0, 0);
+						// spiral.object3D.scale.set(0.1, 0.1, 0.1);
+						// spiral.object3D.rotation.set(0, 0, 0);
+						// spiral.emit('rotate');
+						//
+						// markerEl.appendChild(spiral);
+						//
+						//
+						// // Torus 1
+						// // textEl.setAttribute('gltf-model','#monster');
+						// // textEl.setAttribute('gps-entity-place', `latitude: ${position.coords.latitude}; longitude: ${position.coords.longitude}`);
+						// // textEl.setAttribute('gps-entity-place', `latitude: ${lat}; longitude: ${long}`);
+						//
+						// torus1.setAttribute('obj-model', {
+						// 	obj: '#torus_gold_oldest-obj',
+						// 	mtl: '#torus_gold_oldest-mtl'
+						// });
+						// torus1.object3D.position.set(1, 0, 0);
+						// torus1.object3D.scale.set(0.1, 0.1, 0.1);
+						// torus1.object3D.rotation.set(0, 0, 0);
+						//
+						//
+						// sphere1.setAttribute('obj-model', {
+						// 	obj: '#sphere_gold_oldest-obj',
+						// 	mtl: '#sphere_gold_oldest-mtl'
+						// });
+						// sphere1.object3D.position.set(1, 0, 0);
+						// sphere1.object3D.scale.set(0.05, 0.05, 0.05);
+						// sphere1.object3D.rotation.set(0, 0, 0);
+						//
+						// sphere1.setAttribute('animation', {
+						//   property: 'rotation',
+						//   to: '0 360 0',
+						//   loop: true,
+						// 	dur: 2000
+						// });
+						//
+						//
+						// markerEl.appendChild(torus1);
+						// markerEl.appendChild(sphere1);
+						// // markerEl.appendChild(torus1_parent);
+						//
+						//
+						// // Torus 2
+						// // textEl1.setAttribute('gps-entity-place', `latitude: ${position.coords.latitude}; longitude: ${position.coords.longitude}`);
+						// // textEl1.setAttribute('gps-entity-place', `latitude: ${lat}; longitude: ${long}`);
+						// torus2.setAttribute('obj-model', {
+						// 	obj: '#torus_gold_oldest-obj',
+						// 	mtl: '#torus_gold_oldest-mtl'
+						// });
+						// torus2.object3D.position.set(1.3, 0, 0);
+						// torus2.object3D.scale.set(0.2, 0.2, 0.2);
+						// torus2.object3D.rotation.set(0, 0, 0);
+						//
+						// sphere2.setAttribute('obj-model', {
+						// 	obj: '#sphere_gold_oldest-obj',
+						// 	mtl: '#sphere_gold_oldest-mtl'
+						// });
+						// sphere2.object3D.position.set(1.3, 0, 0);
+						// sphere2.object3D.scale.set(0.05, 0.05, 0.05);
+						// sphere2.object3D.rotation.set(0, 0, 0);
+						//
+						//
+						// markerEl.appendChild(torus2);
+						// markerEl.appendChild(sphere2);
+						// // markerEl.appendChild(torus2_parent);
+						//
+						//
+						//
+						// // Torus 3
+						// // textEl2.setAttribute('gps-entity-place', `latitude: ${position.coords.latitude}; longitude: ${position.coords.longitude}`);
+						// // textEl2.setAttribute('gps-entity-place', `latitude: ${lat}; longitude: ${long}`);
+						// torus3.setAttribute('obj-model', {
+						// 	obj: '#torus_gold_oldest-obj',
+						// 	mtl: '#torus_gold_oldest-mtl'
+						// });
+						// torus3.object3D.position.set(1.6, 0, 0);
+						// torus3.object3D.scale.set(0.3, 0.3, 0.3);
+						// torus3.object3D.rotation.set(0, 0, 0);
+						//
+						// sphere3.setAttribute('obj-model', {
+						// 	obj: '#sphere_gold_oldest-obj',
+						// 	mtl: '#sphere_gold_oldest-mtl'
+						// });
+						// sphere3.object3D.position.set(1.6, 0, 0);
+						// sphere3.object3D.scale.set(0.05, 0.05, 0.05);
+						// sphere3.object3D.rotation.set(0, 0, 0);
+						//
+						//
+						// markerEl.appendChild(torus3);
+						// markerEl.appendChild(sphere3);
+						// // markerEl.appendChild(torus3_parent);
+						//
+						//
+						// // console.log('Model component registered successfully!' , ` latitude: ${lat}; longitude: ${long}`);
+						// console.log('Model component registered successfully!' );
+						// console.log(markerEl);
 				}
 
 				// var cameraEl = document.querySelector('#camera');
@@ -185,18 +185,132 @@ AFRAME.registerComponent('registerevents', {
 						console.log('Marker Found: ', markerId);
 
 
-            // var cameraEl = document.querySelector('#camera');
-            // var worldPos = new THREE.Vector3();
-            // worldPos.setFromMatrixPosition(cameraEl.object3D.matrixWorld);
-            // console.log("yang world pos: ", worldPos.x);
+						var sceneElement = document.querySelector('a-scene');
 
-						// tick: function () {
-					  //   var cameraEl = this.el.sceneEl.camera.el;
-					  //   console.log("yang tick", cameraEl.getAttribute('position'));
-					  //   console.log("yang tick" , cameraEl.getAttribute('rotation'));
-						//
-					  //   // Do something.
-					  // }
+						//Adding element
+						var spiral = document.createElement('a-entity');
+
+
+						var torus1_parent = document.createElement('a-entity');
+						var torus1 = document.createElement('a-entity');
+						var sphere1 = document.createElement('a-entity');
+
+						var torus2_parent = document.createElement('a-entity');
+						var torus2 = document.createElement('a-entity');
+						var sphere2 = document.createElement('a-entity');
+
+						var torus3_parent = document.createElement('a-entity');
+						var torus3 = document.createElement('a-entity');
+						var sphere3 = document.createElement('a-entity');
+
+						// OBJ model
+
+						// Spiral
+						// textEl.setAttribute('gltf-model','#monster');
+						// spiral.setAttribute('gps-entity-place', `latitude: ${position.coords.latitude}; longitude: ${position.coords.longitude}`);
+						// spiral.setAttribute('gps-entity-place', `latitude: ${lat}; longitude: ${long}`);
+						spiral.setAttribute('obj-model', {
+							obj: '#spiral-obj',
+							mtl: '#spiral-mtl'
+						});
+						spiral.object3D.position.set(0, 0, 0);
+						spiral.object3D.scale.set(0.1, 0.1, 0.1);
+						spiral.object3D.rotation.set(0, 0, 0);
+						spiral.emit('rotate');
+
+						sceneElement.appendChild(spiral);
+
+
+						// Torus 1
+						// textEl.setAttribute('gltf-model','#monster');
+						// textEl.setAttribute('gps-entity-place', `latitude: ${position.coords.latitude}; longitude: ${position.coords.longitude}`);
+						// textEl.setAttribute('gps-entity-place', `latitude: ${lat}; longitude: ${long}`);
+
+						torus1.setAttribute('obj-model', {
+							obj: '#torus_gold_oldest-obj',
+							mtl: '#torus_gold_oldest-mtl'
+						});
+						torus1.object3D.position.set(1, 0, 0);
+						torus1.object3D.scale.set(0.1, 0.1, 0.1);
+						torus1.object3D.rotation.set(0, 0, 0);
+
+
+						sphere1.setAttribute('obj-model', {
+							obj: '#sphere_gold_oldest-obj',
+							mtl: '#sphere_gold_oldest-mtl'
+						});
+						sphere1.object3D.position.set(1, 0, 0);
+						sphere1.object3D.scale.set(0.05, 0.05, 0.05);
+						sphere1.object3D.rotation.set(0, 0, 0);
+
+						sphere1.setAttribute('animation', {
+							property: 'rotation',
+							to: '0 360 0',
+							loop: true,
+							dur: 2000
+						});
+
+
+						sceneElement.appendChild(torus1);
+						sceneElement.appendChild(sphere1);
+						// markerEl.appendChild(torus1_parent);
+
+
+						// Torus 2
+						// textEl1.setAttribute('gps-entity-place', `latitude: ${position.coords.latitude}; longitude: ${position.coords.longitude}`);
+						// textEl1.setAttribute('gps-entity-place', `latitude: ${lat}; longitude: ${long}`);
+						torus2.setAttribute('obj-model', {
+							obj: '#torus_gold_oldest-obj',
+							mtl: '#torus_gold_oldest-mtl'
+						});
+						torus2.object3D.position.set(1.3, 0, 0);
+						torus2.object3D.scale.set(0.2, 0.2, 0.2);
+						torus2.object3D.rotation.set(0, 0, 0);
+
+						sphere2.setAttribute('obj-model', {
+							obj: '#sphere_gold_oldest-obj',
+							mtl: '#sphere_gold_oldest-mtl'
+						});
+						sphere2.object3D.position.set(1.3, 0, 0);
+						sphere2.object3D.scale.set(0.05, 0.05, 0.05);
+						sphere2.object3D.rotation.set(0, 0, 0);
+
+
+						sceneElement.appendChild(torus2);
+						sceneElement.appendChild(sphere2);
+						// markerEl.appendChild(torus2_parent);
+
+
+
+						// Torus 3
+						// textEl2.setAttribute('gps-entity-place', `latitude: ${position.coords.latitude}; longitude: ${position.coords.longitude}`);
+						// textEl2.setAttribute('gps-entity-place', `latitude: ${lat}; longitude: ${long}`);
+						torus3.setAttribute('obj-model', {
+							obj: '#torus_gold_oldest-obj',
+							mtl: '#torus_gold_oldest-mtl'
+						});
+						torus3.object3D.position.set(1.6, 0, 0);
+						torus3.object3D.scale.set(0.3, 0.3, 0.3);
+						torus3.object3D.rotation.set(0, 0, 0);
+
+						sphere3.setAttribute('obj-model', {
+							obj: '#sphere_gold_oldest-obj',
+							mtl: '#sphere_gold_oldest-mtl'
+						});
+						sphere3.object3D.position.set(1.6, 0, 0);
+						sphere3.object3D.scale.set(0.05, 0.05, 0.05);
+						sphere3.object3D.rotation.set(0, 0, 0);
+
+
+						sceneElement.appendChild(torus3);
+						sceneElement.appendChild(sphere3);
+						// markerEl.appendChild(torus3_parent);
+
+
+						// console.log('Model component registered successfully!' , ` latitude: ${lat}; longitude: ${long}`);
+						console.log('Model component registered successfully!' );
+						console.log(sceneElement);
+
 
 			});
 
