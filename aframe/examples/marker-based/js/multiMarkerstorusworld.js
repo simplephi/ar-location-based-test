aafcdc80583bfe8f22158f37febf671d60bd1ff9
simplephi <cleverdarkness@gmail.com>
2022-04-12 13:24:32 +0800

    sphere and position

--- aframe/examples/marker-based/js/multiMarkerstorusworld.js
+++ aframe/examples/marker-based/js/multiMarkerstorusworld.js
@@ -215,12 +215,14 @@ AFRAME.registerComponent('rotation-reader', {
     var position = new THREE.Vector3();
     var quaternion = new THREE.Quaternion();
 
+
     return function () {
       var tes_position = this.el.object3D.getWorldPosition(position);
       var rotation = this.el.object3D.getWorldQuaternion(quaternion);
-			// position.setFromMatrixPosition(this.el.object3D.matrixWorld);
+			position.setFromMatrixPosition(el.object3D.matrixWorld);
 			// console.log("Position from setFromMatrixPosition: " + position.x + " " + position.y + " " + position.z);
-			console.log("Position from getWorldPosition: ", tes_position);
+			// console.log("Position from getWorldPosition: ", tes_position);
+			console.log("Position from getWorldPosition: ", position.setFromMatrixPosition(el.object3D.matrixWorld));
 			// console.log("rotation: ", rotation);
       // position and rotation now contain vector and quaternion in world space.
     };
