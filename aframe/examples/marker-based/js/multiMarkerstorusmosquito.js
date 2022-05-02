87e92b2232b5b3f855de167aa4a90b173c20eddd
simplephi <cleverdarkness@gmail.com>
2022-05-02 14:30:40 +0800

    new gltf mosquito

--- aframe/examples/marker-based/js/multiMarkerstorusmosquito.js
+++ aframe/examples/marker-based/js/multiMarkerstorusmosquito.js
@@ -94,7 +94,7 @@ AFRAME.registerComponent('registerevents', {
 
 
             mosquito.object3D.position.set(0, 1, 0);
-            mosquito.object3D.scale.set(0.5, 0.5, 0.5);
+            mosquito.object3D.scale.set(0.1, 0.1, 0.1);
             mosquito.object3D.rotation.set(0, 0, 0);
 
             // mosquito.setAttribute('animation', {
